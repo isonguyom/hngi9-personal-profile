efd66d8747a1728e7d09a2fcf966260f7b2d9350
isonguyom <isonguyom@gmail.com>
2022-10-28 19:44:16 +0100

    Setup local development environment using create-react-app

---
+++ src/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _client = _interopRequireDefault(require("react-dom/client"));
+
+require("./index.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
