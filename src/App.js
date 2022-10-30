d6008e30f405129b6966a301a85b83abd1ae4398
isonguyom <isonguyom@gmail.com>
2022-10-30 21:21:54 +0100

    Create and export App.js component and App.css style sheet

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+
+
+function App() {
+    return (
+        <h1>Hello, App component</h1>
+    );
+};
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 import './index.css';
 
 
@@ -8,6 +9,5 @@ import './index.css';
 
 
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, HNGI9</h1>);
+root.render(<App/>);
