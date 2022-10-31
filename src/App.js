5aff3cd6bff5a5810edb94d1a43dba35d47d010d
isonguyom <isonguyom@gmail.com>
2022-10-31 07:04:44 +0100

    Create Link component to house link section elements

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import './App.css';
 function Profile() {
     return (
         <div className='Profile-section'>
-            <img src='' id='profile_img' alt='profile picture'/>
+            <img src='' id='profile_img' alt='profile'/>
             <p id='twitter'>@IsonguyoM</p>
             <p id='slack'>Isonguyom</p>
+        </div>
+    );
+};
+
+function Links() {
+    return (
+        <div className='Links'>
+        <h1>Hello, Links</h1>
         </div>
     );
 };
@@ -16,7 +24,10 @@ function Profile() {
 
 function App() {
     return (
+        <div>
         <Profile/>
+        <Links/>
+        </div>
     );
 };
 
