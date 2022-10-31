990789d2bc8bc74fb1fd97645bc8c70b8c0b6bee
isonguyom <isonguyom@gmail.com>
2022-10-31 18:46:22 +0100

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -15,10 +15,23 @@ function Profile() {
     );
 };
 
+function Footer() {
+    return (
+        <div className='Footer'>
+            <Button link='Zuri.com' text='Zuri'/>
+            <p>HNG Internship 9 Front-end Task</p>
+            <Button link='Zuri.com' text='I4G'/>
+        </div>
+    )
+}
+
 function Links() {
     return (
         <div className='Links'>
         <Button link='react.org' text='React' />
+        <Button link='react.org' text='React' />
+        <Button link='react.org' text='React' />
+        <Footer />
         </div>
     );
 };
