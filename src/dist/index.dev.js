b77fac65b7e57b0b262a46a6c8745ba268095a6d
isonguyom <isonguyom@gmail.com>
2022-11-01 21:45:31 +0100

    ignore src/dist folder, create production build

--- src/dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _client = _interopRequireDefault(require("react-dom/client"));
-
-require("./index.css");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
