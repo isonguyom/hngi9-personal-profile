d72a06c706fee3ca33389491e9bc0355738abea5
isonguyom <isonguyom@gmail.com>
2022-11-01 05:53:06 +0100

    Add styles and images to page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import image from './me.jpg'
 import zuriLogo from './zuri_logo.svg'
-import i4g from './i4g_logo.png'
+import i4g from './i4g_logo.svg'
+import slack from './slack.svg'
+import github from './github.svg'
 import './App.css';
 
 function Button(props) {
-    return <a href={ props.link } id={ props.id }>{ props.text }</a>
+    return <a className='btn' href={ props.link } id={ props.id } type='button'>{ props.text }</a>
 }
 
 function Profile() {
     return (
-        <div className='Profile-section'>
+        <div className='Profile'>
             <img src={ image } id='profile_img' alt='Martin'/>
-            <p id='twitter'>@IsonguyoM</p>
-            <p id='slack'>Isonguyom</p>
+            <h2 id='name'>Martin Isonguyo</h2>
+            <div className='usernames'>
+            <span id='twitter'><b>Twitter: </b>IsonguyoM</span>
+            <span id='slack'><b>Slack: </b>Isonguyom</span>
+            </div>
         </div>
     );
 };
 
-function Footer() {
-    return (
-        <div className='Footer'>
-            <a href='https://internship.zuri.team/'><img src={ zuriLogo } alt='Zuri Internship'/></a>
-            <p>HNG Internship 9 Front-end Task</p>
-            <a href='https://internship.zuri.team/'><img src={ i4g } alt='I4G'/></a>
-        </div>
-    )
-}
+
 
 function Links() {
     return (
@@ -36,10 +33,24 @@ function Links() {
         <Button link='https://books.zuri.team/python-for-beginners' id='book__python' text='Python Books' />
         <Button link='https://background.zuri.team' id='pitch' text='Background checks on coders' />
         <Button link='https://books.zuri.team/design-rules' id='book__design' text='Design Books' />
+        <div className='links_footer'>
+            <a className='social_link' href='https://ingressive.org/'><img src={ slack } alt='slack'/></a>
+            <a className='social_link' href='https://ingressive.org/'><img src={ github } alt='github' /></a>
+        </div>
         </div>
     );
 };
 
+
+function Footer() {
+    return (
+        <div className='Footer'>
+            <a href='https://internship.zuri.team/'><img src={ zuriLogo } alt='Zuri Internship'/></a>
+            <p>HNG Internship 9 Front-end Task</p>
+            <a href='https://ingressive.org/'><img src={ i4g } alt='I4G'/></a>
+        </div>
+    );
+};
 
 function App() {
     return (
