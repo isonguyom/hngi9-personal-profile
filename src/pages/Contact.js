610ab35ce4033dc1a8851b0057f254bdf3957a14
isonguyom <isonguyom@gmail.com>
2022-11-04 08:21:11 +0100

    feat: Add contact form elements to Contact component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,10 +2,32 @@ import React from 'react';
 
 
 function Contact() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert('Form submitted')
+      };
+
 return (
-<div>
+<form className='Contact' onSubmit={ handleSubmit }>
     <h2>Hello, Contact</h2>
-</div>
+    <label>First name
+        <input type="text" id='first_name' placeholder='Enter your first name'/>
+      </label>
+    <label>Last name
+        <input type="text" id='last_name' placeholder='Enter your last name'/>
+      </label>
+    <label>Email
+        <input type="email" id='email' placeholder='yourname@gmail.com'/>
+      </label>
+    <label>Message
+    <textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..." />
+      </label>
+    <label>
+    <input type='checkbox' id='aggreement'/>
+    You agree to providing your data to Martin Isonguyo, who may contact you.
+      </label>
+    <button type='submit' id='btn__submit'>Submit</button>
+</form>
 );
 };
 
