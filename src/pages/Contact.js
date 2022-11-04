c3e488a80a698078a595393bd0aa6d34ac4e212c
isonguyom <isonguyom@gmail.com>
2022-11-04 10:10:31 +0100

    style: Style contact form elements

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,3 @@
-import React from 'react';
-
-
 function Contact() {
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -8,25 +5,33 @@ function Contact() {
       };
 
 return (
-<form className='Contact' onSubmit={ handleSubmit }>
-    <h2>Hello, Contact</h2>
-    <label>First name
-        <input type="text" id='first_name' placeholder='Enter your first name'/>
-      </label>
-    <label>Last name
-        <input type="text" id='last_name' placeholder='Enter your last name'/>
-      </label>
-    <label>Email
-        <input type="email" id='email' placeholder='yourname@gmail.com'/>
-      </label>
-    <label>Message
-    <textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..." />
-      </label>
-    <label>
-    <input type='checkbox' id='aggreement'/>
-    You agree to providing your data to Martin Isonguyo, who may contact you.
-      </label>
-    <button type='submit' id='btn__submit'>Submit</button>
+    <form className='Contact' onSubmit={handleSubmit}>
+        <h2 id='contact_form_title'>Contact Me</h2>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <div className='names-wrapper'>
+        <div className='form-control'>
+            <label htmlFor='first_name'>First name</label>
+                <input type="text" id='first_name' placeholder='Enter your first name' /> 
+        </div>
+        <div className='form-control'>
+            <label htmlFor='last_name'>Last name</label>
+                <input type="text" id='last_name' placeholder='Enter your last name' />  
+        </div>
+        </div>
+        <div className='form-control'>
+            <label htmlFor='email'>Email</label>
+                <input type="email" id='email' placeholder='yourname@gmail.com' /> 
+        </div>
+        <div className='form-control'>
+            <label htmlFor='message'>Message</label>
+                <textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..." />   
+        </div>
+        <div className='form-control'>  
+           <label>
+           <input type='checkbox' id='aggreement' />
+        You agree to providing your data to Martin Isonguyo, who may contact you.</label>
+    </div>
+    <button type='submit' id='btn__submit'>Send message</button>
 </form>
 );
 };
