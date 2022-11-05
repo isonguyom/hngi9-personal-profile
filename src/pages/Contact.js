6eb83a19ef2b3ca8284b7953c473331807279067
isonguyom <isonguyom@gmail.com>
2022-11-05 04:05:53 +0100

    feat: Add state management to form fields

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import Footer from "../components/Footer";
 
+ const userInputs = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+    agree: false
+    }
+
 function Contact() {
+   
+    const [inputs, setInputs] = useState(userInputs);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs({ ...inputs, [name]: value });
+      };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert('Form submitted')
-      };
+        console.log(inputs)
+    };
+
 
 return (
     <div className="Contact">
@@ -14,24 +33,24 @@ return (
         <div className='names-wrapper'>
         <div className='form-control'>
             <label htmlFor='first_name'>First name</label>
-                <input type="text" id='first_name' placeholder='Enter your first name' /> 
+                <input type="text" name="firstName" id='first_name' placeholder='Enter your first name' onChange={handleChange} /> 
         </div>
         <div className='form-control'>
             <label htmlFor='last_name'>Last name</label>
-                <input type="text" id='last_name' placeholder='Enter your last name' />  
+                <input type="text" name="lastName" id='last_name' placeholder='Enter your last name' onChange={handleChange} />  
         </div>
         </div>
         <div className='form-control'>
             <label htmlFor='email'>Email</label>
-                <input type="email" id='email' placeholder='yourname@gmail.com' /> 
+                <input type="email" name="email" id='email' placeholder='yourname@gmail.com' onChange={handleChange}/> 
         </div>
         <div className='form-control'>
             <label htmlFor='message'>Message</label>
-                <textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..." />   
+                <textarea id='message' name="message" rows={8} placeholder="Send me a message and I'll reply you as soon as possible..." onChange={handleChange} />   
         </div>
         <div className='form-control'>  
            <label>
-           <input type='checkbox' id='aggreement' />
+           <input type='checkbox' name="agree" id='aggreement' onChange={handleChange} />
         You agree to providing your data to Martin Isonguyo, who may contact you.</label>
     </div>
     <button type='submit' id='btn__submit'>Send message</button>
